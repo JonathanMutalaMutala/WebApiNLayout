81ee8810e6a4c7eb69e27aff8e09904f6d1ad814
Jonathan Mutala <jmutala@royer.com>
2022-10-20 12:02:09 -0400

    Couche access aux données

---
+++ DataAccessLayer/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using DataAccessLayer.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Data
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base() { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public List<AppUser> appUsers { get; set; }
+    }
+}
---
+++ DataAccessLayer/Repositories/RepositoryAppUser.cs
@@ -0,0 +1,112 @@
+﻿using DataAccessLayer.Contract;
+using DataAccessLayer.Data;
+using DataAccessLayer.Models;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class RepositoryAppUser : IRepository<AppUser>
+    {
+        private readonly ILogger _logger; 
+        private readonly AppDbContext _appDbContext; 
+        
+        public RepositoryAppUser(ILogger<AppUser> logger)
+        {
+            _logger = logger;
+        }
+        public async Task<AppUser> Create(AppUser appUser)
+        {
+            try
+            {
+                if(appUser != null)
+                {
+                    var obj = _appDbContext.Add<AppUser>(appUser);
+                    await _appDbContext.SaveChangesAsync();
+                    return obj.Entity; 
+                }else
+                {
+                    return null; 
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public void Delete(AppUser appuser)
+        {
+            try
+            {
+                if(appuser != null)
+                {
+                    var obj = _appDbContext.Remove(appuser); 
+                    if(obj != null)
+                    {
+                        _appDbContext.SaveChangesAsync(); 
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public void Update(AppUser appUser)
+        {
+            try
+            {
+                if(appUser != null)
+                {
+                    var obj = _appDbContext.Update(appUser); 
+                    if( obj != null) _appDbContext.SaveChangesAsync();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public IEnumerable<AppUser> GetAll()
+        {
+            try
+            {
+                var obj = _appDbContext.appUsers.ToList();
+                if (obj != null) return obj;
+                else return null;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public AppUser GetById(int Id)
+        {
+            try
+            {
+                if (Id != null)
+                {
+                    var Obj = _appDbContext.appUsers.FirstOrDefault(x => x.Id == Id);
+                    if (Obj != null) return Obj;
+                    else return null;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+    }
+}
